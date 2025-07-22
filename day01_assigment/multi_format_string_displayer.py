eea6162ae898d8f4b3bd99450028b7895d95aa23
Parasite <ansuman0026@gmail.com>
2025-07-23 02:43:38 +0530

    Add multi-format string displayer solution

---
+++ day01_assigment/multi_format_string_displayer.py
@@ -0,0 +1,9 @@
+name = "Alice"
+age = 28
+city = "Delhi"
+
+print(f"My name is {name}, I am {age} years old, and I live in {city}.")
+
+print("My name is {}, I am {} years old, and I live in {}.".format(name, age, city))
+
+print("My name is %s, I am %d years old, and I live in %s." % (name, age, city))
